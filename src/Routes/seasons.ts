25308906e2e4aeadc28fb0edad1eba9671db65a9
Fady H. Wahba <fadyw@microsoft.com>
2019-05-17 12:10:22 -0700

    Addressing CR comments

--- src/Routes/seasons.ts
+++ src/Routes/seasons.ts
@@ -13,7 +13,6 @@ const router = express.Router();
 const dataProvider: IDataProvider = new SqlDataProvider(Configuration.sqlConfig);
 
 router.get('/', (req: express.Request, res: express.Response) => {
-    console.log("Fetching all seasons");
     dataProvider.getSeasonList()
         .then((result) => {
             Networking.setResponseHeaders(res);
@@ -22,7 +21,6 @@ router.get('/', (req: express.Request, res: express.Response) => {
 });
 
 router.get("/:seasonId", (req: express.Request, res: express.Response) => {
-    console.log(`Fetching seasonid: ${req.params.seasonId}`);
     dataProvider.getSeason(req.params.seasonId)
         .then((result) => {
             Networking.setResponseHeaders(res);

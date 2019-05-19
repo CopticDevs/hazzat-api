84fea84718652fb2f435afc23f4c04397ffe39b8
Fady H. Wahba <fadyw@microsoft.com>
2019-05-19 13:53:20 -0700

    Replaced .then with async/await

--- src/Routes/seasons.ts
+++ src/Routes/seasons.ts
@@ -7,21 +7,17 @@ import { IDataProvider } from "../DataProviders/IDataProvider";
 import { SqlDataProvider } from "../DataProviders/SqlDataProvider/SqlDataProvider";
 const router = express.Router();
 
-// TODO: Add a factory pattern
+// TODO: Add a factory pattern or dependency injection
 const dataProvider: IDataProvider = new SqlDataProvider(Configuration.dbConnectionString);
 
-router.get('/', (req: express.Request, res: express.Response) => {
-    dataProvider.getSeasonList()
-        .then((result) => {
-            res.send(result);
-        });
+router.get('/', async (req: express.Request, res: express.Response) => {
+    const result = await dataProvider.getSeasonList();
+    res.send(result);
 });
 
-router.get("/:seasonId", (req: express.Request, res: express.Response) => {
-    dataProvider.getSeason(req.params.seasonId)
-        .then((result) => {
-            res.send(result);
-    });
+router.get("/:seasonId", async (req: express.Request, res: express.Response) => {
+    const result = dataProvider.getSeason(req.params.seasonId);
+    res.send(result);
 });
 
 export default router;

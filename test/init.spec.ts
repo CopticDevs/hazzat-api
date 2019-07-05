7725229a44a7f0a213d4c7a6beb8fde265f65ff3
Beshoy Hanna <beshoyhanna@outlook.com>
2019-07-04 21:49:03 -0400

    [WIP] initialize unit testing framework (#38)
    * initialize unit testing framework - mocha
    
    * moved test dir out of src

---
+++ test/init.spec.ts
@@ -0,0 +1,18 @@
+var assert = require('assert');
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should be true because javascript', function () {
+      assert.equal(1,true);
+    });
+  });
+});

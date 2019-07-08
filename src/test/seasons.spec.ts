d278c77869219a06fa1fcdb8a92fd46141aa495f
Fady H. Wahba <fadyw@microsoft.com>
2019-07-07 20:41:29 -0700

    Added seasons tests

--- src/test/seasons.spec.ts
+++ src/test/seasons.spec.ts
@@ -1,7 +1,10 @@
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
 import server = require("../app");
+import { ErrorCodes } from "../Common/Errors";
 const should = chai.should();
+
+process.env.NODE_ENV = "test";
 
 chai.use(chaiHttp);
 
@@ -14,6 +17,55 @@ describe("Seasons", () => {
                     res.should.have.status(200);
                     res.body.should.be.a("array");
                     res.body.length.should.be.not.eql(0);
+                    done();
+                });
+        });
+
+        it("should get a single season", (done) => {
+            chai.request(server)
+                .get("/seasons/1")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("name");
+                    res.body.should.have.property("order");
+                    res.body.should.have.property("reasonId");
+                    res.body.should.have.property("verse");
+                    done();
+                });
+        });
+
+        it("should return a 404 for non existing seasons", (done) => {
+            chai.request(server)
+                .get("/seasons/1234")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("errorCode");
+                    res.body.should.have.property("errorCode").eql(ErrorCodes[ErrorCodes.NotFoundError]);
+                    res.body.should.have.property("message");
+                    done();
+                });
+        });
+
+        it("should return a 404 for negative season ids", (done) => {
+            chai.request(server)
+                .get("/seasons/-1")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("message");
+                    done();
+                });
+        });
+
+        it("should return a 404 for non integer season ids", (done) => {
+            chai.request(server)
+                .get("/seasons/badInput")
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("message");
                     done();
                 });
         });

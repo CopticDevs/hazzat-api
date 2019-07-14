bf611012683fa7fb0e7655e22737aaa2e34488fd
Fady H. Wahba <fadyw@microsoft.com>
2019-07-13 19:46:11 -0700

    Updated paths

--- .build/createDeploymentArchive.ps1
+++ .build/createDeploymentArchive.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path ..\src\dist\*, ..\src\node_modules, ..\src\Web.config, ..\src\Web.Debug.config -CompressionLevel Optimal -DestinationPath $env:ARCHIVE_FILE_PATH
+Compress-Archive -Path $env:ENLISTMENT_ROOT\src\dist\*, $env:ENLISTMENT_ROOT\src\node_modules, $env:ENLISTMENT_ROOT\src\Web.config, $env:ENLISTMENT_ROOT\src\Web.Debug.config -CompressionLevel Optimal -DestinationPath $env:ARCHIVE_FILE_PATH

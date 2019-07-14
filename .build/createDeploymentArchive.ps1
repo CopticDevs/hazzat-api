1800399f2825805aa44ca0b02ef92c558c9702c8
Fady H. Wahba <fadyw@microsoft.com>
2019-07-13 20:35:55 -0700

    Updated paths

--- .build/createDeploymentArchive.ps1
+++ .build/createDeploymentArchive.ps1
@@ -1,3 +1,3 @@
-Get-ChildItem -Path $env:ENLISTMENT_ROOT\src -Recurse
+Get-ChildItem -Path $env:OUTPUT_ROOT\src -Recurse
 
 Compress-Archive -Path $env:OUTPUT_ROOT\dist\*, $env:ENLISTMENT_ROOT\src\node_modules, $env:ENLISTMENT_ROOT\src\Web.config, $env:ENLISTMENT_ROOT\src\Web.Debug.config -CompressionLevel Optimal -DestinationPath $env:ARCHIVE_FILE_PATH

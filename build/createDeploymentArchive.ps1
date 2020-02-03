0168c5109aacc92a5a89aa53ec93597bdef90281
beshoy hanna <beshoyhanna@Beshoys-MacBook.hsd1.mi.comcast.net>
2020-02-03 13:51:03 -0500

    revert

--- build/createDeploymentArchive.ps1
+++
@@ -1,1 +0,0 @@
-Compress-Archive -Path $env:ENLISTMENT_ROOT\src\dist\*, $env:ENLISTMENT_ROOT\src\node_modules, $env:ENLISTMENT_ROOT\src\Web.config, $env:ENLISTMENT_ROOT\src\Web.Debug.config, $env:ENLISTMENT_ROOT\build\createDeploymentArchive.ps1 -CompressionLevel Optimal -DestinationPath $env:ARCHIVE_FILE_PATH

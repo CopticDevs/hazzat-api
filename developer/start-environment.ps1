7f80244014934bee633cad683aed6ff0a99cdc8c
Fady H. Wahba <fadyw@microsoft.com>
2021-01-30 15:21:36 -0800

    Added different environments

--- developer/start-environment.ps1
+++ developer/start-environment.ps1
@@ -1,11 +1,49 @@
-$keyvaultName = 'hazzat-api-ppe'
-$secretName = 'hazzat-db-dev-ro-connectionString'
+param(
+    [ValidateSet("DEV", "PPE", "PROD")]
+    [string]$DeploymentEnvironment = "DEV",
+    [bool]$StartVisualStudio = $true
+)
+
+# The values of some settings in the different environments.
+$environmentSettings = @{
+    "DEV" = @{
+        "resourceGroupName" =  "hazzat-api-dev";
+        "keyvaultName"      = "hazzat-api-dev";
+        "secretName"        = "hazzat-db-ro-connectionString";
+    };
+    "PPE" = @{
+        "resourceGroupName" =  "hazzat-api-ppe";
+        "keyvaultName"      = "hazzat-api-ppe";
+        "secretName"        = "hazzat-db-ro-connectionString";
+    };
+    "PROD" = @{
+        "resourceGroupName" =  "hazzat-api";
+        "keyvaultName"      = "hazzat-api";
+        "secretName"        = "hazzat-db-ro-connectionString";
+    };
+}
+
 $slnPath = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition) + '\..\src\hazzat-api.sln'
 
+<#
+    .Synopsis
+      Starts the solution in Visual Studio 2019
+#>
+function StartDevEnv([string]$dbConnectionString) {
+    $env:CUSTOMCONNSTR_DBCONNECTIONSTRING=$dbConnectionString
+
+    & 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe' $slnPath
+}
+
+Write-Output "Using environment '$DeploymentEnvironment'"
+$keyvaultName = $environmentSettings[$DeploymentEnvironment]["keyvaultName"]
+$secretName = $environmentSettings[$DeploymentEnvironment]["secretName"]
+
 Connect-AzureRmAccount
-$dbString = Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name $secretName
+$dbConnectionString = (Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name $secretName).SecretValueText
+Write-Output "set CUSTOMCONNSTR_DBCONNECTIONSTRING=$dbConnectionString"
 
-$env:CUSTOMCONNSTR_DBCONNECTIONSTRING=$dbString.SecretValueText
-Write-Output "set CUSTOMCONNSTR_DBCONNECTIONSTRING=$($env:CUSTOMCONNSTR_DBCONNECTIONSTRING)"
-
-& 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe' $slnPath
+if ($StartVisualStudio) {
+    Write-Output "Starting Visual Studio 2019 for $slnPath"
+    StartDevEnv $dbConnectionString
+}

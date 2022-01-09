fe4a57f1c0179d7afb640cfa83a60eccc1c06cfd
Fady Wahba <fadyw@microsoft.com>
2022-01-08 19:02:01 -0800

    Added Integration tests

--- src/test/IntegrationTests/tunes.spec.ts
+++ src/test/IntegrationTests/tunes.spec.ts
@@ -62,4 +62,110 @@ describe("Tunes controller", () => {
                 });
         });
     });
+
+    describe("/GET all tune seasons", () => {
+        it("should get all tune seasons", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1/${ResourceTypes.Seasons}`)
+                .end((err, res) => {
+                    Validators.validateArrayChaiResponse(res);
+                    Validators.validateTuneSeason(res.body[0]);
+                    done();
+                });
+        });
+
+        it("should return a 404 for negative tune ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/-1/${ResourceTypes.Seasons}`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return a 404 for non integer tune ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/badInput/${ResourceTypes.Seasons}`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return an empty array for non existing tune ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1234/${ResourceTypes.Seasons}`)
+                .end((err, res) => {
+                    Validators.validateArrayChaiResponse(res, true);
+                    done();
+                });
+        });
+    });
+
+    describe("/GET a tune season", () => {
+        it("should get a tune season", (done) => {
+            const resourceId = `/${ResourceTypes.Tunes}/1/${ResourceTypes.Seasons}/33`;
+            chai.request(server)
+                .get(resourceId)
+                .end((err, res) => {
+                    Validators.validateObjectChaiResponse(res);
+                    Validators.validateTuneSeason(res.body, resourceId);
+                    done();
+                });
+        });
+
+        it("should return a 404 for negative tune ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/-1/${ResourceTypes.Seasons}/33`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return a 404 for negative season ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1/${ResourceTypes.Seasons}/-1`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return a 404 for non integer tune ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/badInput/${ResourceTypes.Seasons}/33`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return a 404 for non integer season ids", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1/${ResourceTypes.Seasons}/badInput`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404);
+                    done();
+                });
+        });
+
+        it("should return a 404 for non existing tune id", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1234/${ResourceTypes.Seasons}/33`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404, ErrorCodes.NotFoundError);
+                    done();
+                });
+        });
+
+        it("should return a 404 for non existing season", (done) => {
+            chai.request(server)
+                .get(`/${ResourceTypes.Tunes}/1/${ResourceTypes.Seasons}/1234`)
+                .end((err, res) => {
+                    Validators.validateErrorChaiResponse(res, 404, ErrorCodes.NotFoundError);
+                    done();
+                });
+        });
+    });
 });
